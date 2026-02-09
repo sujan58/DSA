7a9074ee16dadc8689586d70b3f51709ee770b5a
sujan58 <60571159+sujan58@users.noreply.github.com>
2026-02-09 12:42:13 +0530

    Time: 0 ms (100%), Space: 41.9 MB (93.33%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,17 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
+        // if(n<=1){
+        //     return n;
+        // }
+        // int[] dp = new int[n];
+        // dp[0]=1;
+        // dp[1]=2;
+        // for(int i=2;i<n;i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // for(int i=0;i<n;i++){
+        //     System.out.print(dp[i]+" ");
+        // }
+        // return dp[n-1];
         if(n<=1){
             return n;
         }
-        int[] dp = new int[n];
-        dp[0]=1;
-        dp[1]=2;
+        int step_1=1;
+        int step_2=2;
         for(int i=2;i<n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            int temp=step_1+step_2;
+            step_1=step_2;
+            step_2=temp;
         }
-        for(int i=0;i<n;i++){
-            System.out.print(dp[i]+" ");
-        }
-        return dp[n-1];
+        return step_2;
     }
 }
